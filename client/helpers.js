2df9622d7ebbdb3ca9bd45fddbb26443077cce64
Sadiq Okocha <sokocha@gmail.com>
2015-05-10 11:02:34 +0100

    completes application

--- client/helpers.js
+++ client/helpers.js
@@ -3,3 +3,27 @@ Template.problemsAll.helpers({
     return Problems.find();
   }
 });
+
+Template.submitProblem.helpers({
+  problem: function(){
+    return Session.get('problem');
+  },
+  why1: function(){
+    return Session.get('why1');
+  },
+  why2: function(){
+    return Session.get('why2');
+  },
+  why3: function(){
+    return Session.get('why3');
+  },
+  why4: function(){
+    return Session.get('why4');
+  },
+  why5: function(){
+    return Session.get('why5');
+  },
+  solution: function() {
+    return Session.get('solution');
+  }
+});
